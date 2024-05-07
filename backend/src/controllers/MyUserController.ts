a03ffe515199103c559b586e91b84af1435a8a68
prabirstack <prabirsacademy@gmail.com>
2024-05-07 10:40:40 +0530

    Protected user profile router

--- backend/src/controllers/MyUserController.ts
+++ backend/src/controllers/MyUserController.ts
@@ -1,4 +1,4 @@
-import { Request, Response } from 'express'
+import { NextFunction, Request, Response } from 'express'
 import User from '../models/user'
 
 const createCurrentUser = async (req: Request, res: Response) => {
@@ -43,7 +43,22 @@ const updateCurrentUser = async (req: Request, res: Response) => {
   }
 }
 
+const getCurrentUser = async (req: Request, res: Response) => {
+  try {
+    const currentUser = await User.findOne({ _id: req.userId })
+    if (!currentUser) {
+      return res.status(404).json({ message: 'User not found' })
+    }
+
+    res.json(currentUser)
+  } catch (error) {
+    console.log(error)
+    return res.status(500).json({ message: 'Something went wrong' })
+  }
+}
+
 export default {
   createCurrentUser,
   updateCurrentUser,
+  getCurrentUser,
 }
